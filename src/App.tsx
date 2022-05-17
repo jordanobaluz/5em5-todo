97bd2039aeb6a5b97da3a4766be42182ca0b9a8f
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-17 20:29:11 -0300

    created types with the datas, created new styles components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
 const App = () => {
-  return <C.Container>teste</C.Container>;
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: "comprar pão",
+      done: false,
+    },
+    {
+      id: 2,
+      name: "comprar leite",
+      done: false,
+    },
+  ]);
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item) => (
+          <div>{item.name}</div>
+        ))}
+      </C.Area>
+    </C.Container>
+  );
 };
 
 export default App;
