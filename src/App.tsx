a5a319c7542f409114daa70039a20c0329bca22f
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-17 21:23:30 -0300

    created the addArea component with the input component for new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
 import { ListItem } from "./components/ListItem";
+import { AddArea } from "./components/AddArea";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -16,10 +17,23 @@ const App = () => {
       done: false,
     },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  };
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
           <ListItem key={index} item={item} />
