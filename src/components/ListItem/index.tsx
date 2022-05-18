29959a84c647e7155ef1f8b9285db4ce2633c95c
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-17 21:32:31 -0300

    updated the listItem component for handle with the checkbox

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,15 +3,16 @@ import { Item } from "../../types/Item";
 import { useState } from "react";
 type Props = {
   item: Item;
+  onChange: (id: number, done: boolean) => void;
 };
-export const ListItem = ({ item }: Props) => {
+export const ListItem = ({ item, onChange }: Props) => {
   const [isChecked, setIsChecked] = useState(item.done);
   return (
-    <C.Container done={isChecked}>
+    <C.Container done={item.done}>
       <input
         type="checkbox"
-        checked={isChecked}
-        onChange={(e) => setIsChecked(e.target.checked)}
+        checked={item.done}
+        onChange={(e) => onChange(item.id, e.target.checked)}
       />
       <label>{item.name}</label>
     </C.Container>
